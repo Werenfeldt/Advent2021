288cae73a7f03a7b5a075bf13b7886d45f2ebcb9
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-12-01 21:36:33 +0100

    1 December

---
+++ December1/FirstPuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿//First puzzle
+int numberOfIncreases = 0;
+
+int PreviousInt = 0;
+
+bool FirstItem = true;
+
+foreach (var item in System.IO.File.ReadLines(@"../input.txt"))
+{
+    if (FirstItem)
+    {
+        PreviousInt = int.Parse(item);
+        FirstItem = false;
+    }
+    else
+    {
+        var CurrentInt = int.Parse(item);
+
+        if (CurrentInt > PreviousInt)
+        {
+            numberOfIncreases++;
+        }
+
+        PreviousInt = CurrentInt;
+    }
+}
+
+Console.WriteLine(numberOfIncreases);
