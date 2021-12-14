bb228286517ea6154df155251318d23aa2d2278f
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-12-14 07:46:50 +0100

    December 11 and 12

--- December11/FirstPuzzle/Program.cs
+++ December11/FirstPuzzle/Program.cs
@@ -1,2 +1,322 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    //1= start row, 2= mid row, 3= end row
+    static (int, bool, int)[] grid = new (int, bool, int)[100];
+
+    // adjacentFields = { -11, -10, -9, -1, 1, 9, 10, 11 };
+
+    public static int flashes = 0;
+
+    public static void Main()
+    {
+        int index = 0;
+        int row = 0;
+        foreach (var item in System.IO.File.ReadLines(@"../input.txt"))
+        {
+            row = 0;
+            foreach (var ele in item)
+            {
+                var nb = ele.ToString();
+                if (row == 0)
+                {
+                    grid[index] = (int.Parse(nb), false, 1);
+
+                }
+                else if (row == 9)
+                {
+                    grid[index] = (int.Parse(nb), false, 3);
+
+                }
+                else
+                {
+                    grid[index] = (int.Parse(nb), false, 2);
+                }
+                row++;
+                index++;
+            }
+        }
+
+
+
+        SimulateSteps();
+
+        // for (int i = 0; i < grid.Length; i++)
+        // {
+        //     if (grid[i].Item3 == 1)
+        //     {
+        //         Console.WriteLine(i + " Start Row" + grid[i]);
+        //     }
+        //     else if (grid[i].Item3 == 2)
+        //     {
+        //         Console.WriteLine(i + " Mid Row" + grid[i]);
+        //     }
+        //     else if (grid[i].Item3 == 3)
+        //     {
+        //         Console.WriteLine(i + " End Row" + grid[i]);
+        //         Console.WriteLine();
+        //     }
+        // }
+
+        //Console.WriteLine(flashes);
+
+    }
+
+
+    public static void SimulateSteps()
+    {
+        int step = 0;
+        while (!ResetVisit())
+        {
+            IncreaseEnergy();
+            CheckForFlash();
+            step++;
+        }
+
+        Console.WriteLine("Synchornized!! at step: " + step);
+        // for (int j = 0; j < 200; j++)
+        // {
+
+        //     IncreaseEnergy();
+        //     CheckForFlash();
+        // if ((j+1) == 195)
+        // {
+        //     Console.WriteLine("After step: " + (j + 1));
+
+        //     for (int i = 0; i < grid.Length; i++)
+        //     {
+        //         if (grid[i].Item3 == 3)
+        //         {
+        //             Console.Write(grid[i].Item1 + " " + grid[i].Item2);
+        //             Console.WriteLine();
+        //         }
+        //         else
+        //         {
+        //             Console.Write(grid[i].Item1 + " " + grid[i].Item2 + " ");
+        //         }
+        //     }
+        // }
+        // if(ResetVisit()){
+        //     Console.WriteLine("Synchornized!! at step: " + (j+1));
+        //     break;
+        // }
+
+        // }
+    }
+
+
+    public static void IncreaseEnergy()
+    {
+        for (int i = 0; i < grid.Length; i++)
+        {
+            grid[i].Item1 = grid[i].Item1 + 1;
+        }
+    }
+
+
+
+    public static void CheckForFlash()
+    {
+        for (int i = 0; i < grid.Length; i++)
+        {
+            if (grid[i].Item1 > 9 && grid[i].Item2 == false)
+            {
+                grid[i] = (0, true, grid[i].Item3);
+                flashes++;
+
+                FlashAdjacent(i);
+            }
+        }
+    }
+
+    public static bool ResetVisit()
+    {
+        int synconized = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            if (grid[i].Item2)
+            {
+                synconized++;
+            }
+            grid[i].Item2 = false;
+        }
+
+
+        if (synconized == grid.Length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void FlashAdjacent(int i)
+    {
+        if (i < 10)
+        {
+            if (grid[i].Item3 == 1)
+            {
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+                grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+                grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+
+            }
+            else if (grid[i].Item3 == 3)
+            {
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+                grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+                grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+            }
+            else
+            {
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+                grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+                grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+                grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+
+            }
+        }
+        else if (89 < i && i < 100)
+        {
+            if (grid[i].Item3 == 1)
+            {
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+            }
+            else if (grid[i].Item3 == 3)
+            {
+
+
+                grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+            }
+            else
+            {
+                grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+            }
+        }
+        else
+        {
+            if (grid[i].Item3 == 1)
+            {
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+                grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+                grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+            }
+            else if (grid[i].Item3 == 3)
+            {
+                grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+                grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+                grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+            }
+            else
+            {
+                grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+                grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+                grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+                grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+                grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+                grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+                grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+                grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+            }
+        }
+        CheckForFlash();
+    }
+
+    // public static void FlashAdjacent(int i)
+    // {
+    //     if (i < 10)
+    //     {
+    //         if (grid[i].Item3 == 1)
+    //         {
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //             grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+    //             grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+
+    //         }
+    //         else if (grid[i].Item3 == 3)
+    //         {
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //             grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+    //             grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+    //         }
+    //         else
+    //         {
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //             grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+    //             grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+    //             grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+
+    //         }
+    //     }
+    //     else if (89 < i && i < 100)
+    //     {
+    //         if (grid[i].Item3 == 1)
+    //         {
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //         }
+    //         else if (grid[i].Item3 == 3)
+    //         {
+
+
+    //             grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //         }
+    //         else
+    //         {
+    //             grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         if (grid[i].Item3 == 1)
+    //         {
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //             grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+    //             grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+    //         }
+    //         else if (grid[i].Item3 == 3)
+    //         {
+    //             grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //             grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+    //             grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+    //         }
+    //         else
+    //         {
+    //             grid[i - 11].Item1 = grid[i - 11].Item2 ? grid[i - 11].Item1 : grid[i - 11].Item1 + 1;
+    //             grid[i - 10].Item1 = grid[i - 10].Item2 ? grid[i - 10].Item1 : grid[i - 10].Item1 + 1;
+    //             grid[i - 9].Item1 = grid[i - 9].Item2 ? grid[i - 9].Item1 : grid[i - 9].Item1 + 1;
+    //             grid[i - 1].Item1 = grid[i - 1].Item2 ? grid[i - 1].Item1 : grid[i - 1].Item1 + 1;
+    //             grid[i + 1].Item1 = grid[i + 1].Item2 ? grid[i + 1].Item1 : grid[i + 1].Item1 + 1;
+    //             grid[i + 9].Item1 = grid[i + 9].Item2 ? grid[i + 9].Item1 : grid[i + 9].Item1 + 1;
+    //             grid[i + 10].Item1 = grid[i + 10].Item2 ? grid[i + 10].Item1 : grid[i + 10].Item1 + 1;
+    //             grid[i + 11].Item1 = grid[i + 11].Item2 ? grid[i + 11].Item1 : grid[i + 11].Item1 + 1;
+    //         }
+    //     }
+    //     CheckForFlash();
+    // }
+}
---
+++ December12/FirstPuzzle/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Linq;
+public class Program
+{
+
+    //static List<((string, bool, bool)> PathBuilder = new List<((string, bool, bool))>();
+
+    //name, name, visites
+    //static Node[] PathIndex = new Node[20];
+
+    static List<Node> PathIndex = new List<Node>();
+
+    static Node[]? Path;
+
+    static List<string> PathString = new List<string>();
+
+    static string tmpPath = "";
+
+    //name, is uppercase, has been visited  
+    //static Node node;
+
+    public static void Main()
+    {
+        //int pathIndex = 0;
+
+        foreach (var item in System.IO.File.ReadLines(@"../test.txt"))
+        {
+            string[] line = item.Split("-");
+
+            Node firstNode;
+            Node secondNode;
+
+            if (line[0] == "start" || !line[0].Any(char.IsUpper) || line[0] == "end")
+            {
+                firstNode = new Node(line[0], false, false);
+            }
+            else
+            {
+                firstNode = new Node(line[0], true, false);
+            }
+
+            if (line[1] == "start" || !line[1].Any(char.IsUpper) || line[1] == "end")
+            {
+                secondNode = new Node(line[1], false, false);
+            }
+            else
+            {
+                secondNode = new Node(line[1], true, false);
+            }
+
+
+            if (!PathIndex.Exists(x => x.Name == firstNode.Name))
+            {
+                // PathIndex[pathIndex] = firstNode;
+                // pathIndex++;
+                //Console.WriteLine("Does not contain: " + firstNode.Name);
+                if (firstNode.Name != "end")
+                {
+
+                    firstNode.AddPointsAtNode(secondNode);
+                    PathIndex.Add(firstNode);
+                }
+
+            }
+            else
+            {
+
+                //Console.WriteLine("Does contain: " + firstNode.Name);
+                PathIndex.Find(x => x.Name == firstNode.Name).AddPointsAtNode(secondNode);
+            }
+
+            if (!PathIndex.Exists(x => x.Name == secondNode.Name))
+            {
+                // PathIndex[pathIndex] = secondNode;
+                // pathIndex++;
+                //Console.WriteLine("Does not contain: " + secondNode.Name);
+
+
+
+                if (firstNode.Name != "start")
+                {
+                    secondNode.AddPointsAtNode(firstNode);
+                }
+                if (secondNode.Name != "end")
+                {
+                    PathIndex.Add(secondNode);
+                }
+            }
+            else
+            {
+                if (firstNode.Name != "start")
+                {
+
+                    PathIndex.Find(x => x.Name == secondNode.Name).AddPointsAtNode(firstNode);
+                }
+                //Console.WriteLine("Does contain: " + secondNode.Name);
+            }
+
+            Path = PathIndex.ToArray();
+
+        }
+
+        // if (CanVisitNode(0))
+        // {
+        //     VisitNode(0);
+        // }
+
+        //while (!CanVisitNode(PathIndex.ElementAt(0))) ;
+
+        CanVisitNode(PathIndex.ElementAt(0));
+
+        PathString.Add(tmpPath);
+
+        if (PathString != null)
+        {
+
+            foreach (var node in PathString)
+            {
+                Console.WriteLine(node);
+
+            }
+        }
+
+        // if (Path != null)
+        // {
+
+        //     foreach (var node in Path)
+        //     {
+        //         node.PrintInfo();
+
+        //     }
+        // }
+    }
+
+    public static void CanVisitNode(Node node)
+    {
+        if (node.Uppercase && node.Visited || !node.Visited)
+        {
+            Console.WriteLine("This node can be visited: " + node.Name);
+            VisitNode(node);
+            //return true;
+        }
+        else
+        {
+            //return false;
+        }
+    }
+
+    public static void VisitNode(Node node)
+    {
+        Console.WriteLine("This node has been visited: " + node.Name);
+        node.Visited = true;
+        AddToPath(node);
+    }
+
+
+    public static void AddToPath(Node node)
+    {
+        if (visitEle == -1)
+        {
+
+            visitEle++;
+        }
+        Console.WriteLine(visitEle);
+        tmpPath += node.Name;
+        if (node.Name != "end")
+        {
+            if (!node.PointsAt.ElementAt(visitEle).Visited)
+            {
+                Console.WriteLine("hej");
+
+                Console.WriteLine("This node points at: " + node.PointsAt.ElementAt(visitEle).Name);
+                CanVisitNode(node.PointsAt.ElementAt(visitEle));
+            }
+            else
+            {
+                visitEle++;
+            }
+
+
+        }
+    }
+
+    public static int visitEle = -1;
+}
